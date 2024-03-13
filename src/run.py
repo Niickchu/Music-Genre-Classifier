7a5f2d807176c91a50806951cd68c4dd9d3318be
Niick <niickchu@gmail.com>
2024-03-13 11:25:22 -0700

    add temp parsing code

--- src/run.py
+++ src/run.py
@@ -1,7 +1,22 @@
+def temp_parse():
+    import os
+
+    parent_folder_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    datafolder = parent_folder_path + r"\data\dataset_spec"
+
+    input_data = []
+    
+    for genre_folder in os.listdir(datafolder):
+        genre_folder_path = os.path.join(datafolder, genre_folder)
+        for file in os.listdir(genre_folder_path):
+            file_path = os.path.join(genre_folder_path, file)
+            input_data.append((file_path, genre_folder))
+
+    for data in input_data:
+        print(data)
 
 
-def main():
-    pass
 
 if __name__ == "__main__":
-    main()
+    temp_parse()
